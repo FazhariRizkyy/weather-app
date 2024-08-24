33b8ee8c54d1887ee831509c9283b0aa4232018c
Fazhari Rizky <fazharirizkyy@gmail.com>
2024-08-24 22:12:05 +0700

    Remove API key from code

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-require('dotenv').config();
-const apiKey = process.env.API_KEY;
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
